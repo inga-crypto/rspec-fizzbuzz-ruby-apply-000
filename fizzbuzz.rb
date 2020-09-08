d9dac60d56dc652eee596d25369d1e8acb9c3ed1
Inga <ijaanimets@gmail.com>
2020-09-08 00:10:52 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,18 +2,16 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(num)
- if (num % 3 == 0 && num % 5 == 0)
+  if (num % 3 == 0 && num % 5 == 0)
    return "FizzBuzz"
-   #fizz_15
-  #(fizz_15).to eq("FizzBuzz")
-  elsif 
+ elsif 
   num % 3 == 0
-  return "Fizz"
-   elsif
+   return "Fizz"
+ elsif
     num % 5 == 0
-     return "Buzz"
+   return "Buzz"
  else
-  return nil
+   return nil
   end
 end
 
