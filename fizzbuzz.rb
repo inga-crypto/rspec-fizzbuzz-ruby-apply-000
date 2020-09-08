71134cdb33263648904ad9c5512bc36b51304fed
Inga <ijaanimets@gmail.com>
2020-09-08 00:11:28 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -16,22 +16,3 @@ def fizzbuzz(num)
 end
 
 
-=begin def fizzbuzz(int)
-  if int % 3 == 0 # if the number int is divisible by 3
-    "Fizz" # Go fizz
-  end
-end
-=end
-
-=begin def fizzbuzz(num)
- describe "fizzbuzz" do
-  it 'returns "Fizz" when the number is divisible by 3' do
-  if num % 3 == 0
-    fizz_3 = fizzbuzz(3)
-    #fizzbuzz(3) == "Fizz"
- 
-    expect(fizz_3).to eq("Fizz")
-  end
- end
-end
-=end
